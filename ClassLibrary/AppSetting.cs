f2bca7063b3e858e57afc7b64462916bc13324cc
JEFERSON <JEFERSON@JEFERSON-PC>
2017-05-27 00:56:45 -0300

    Teste: Inclusão DB externo para modo Debug e DB na pasta interna

---
+++ ClassLibrary/AppSetting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class AppSetting
+    {
+        Configuration config;
+
+        public AppSetting()
+        {
+            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        }
+
+        public string GetConnectionString(string key)
+        {
+            return config.ConnectionStrings.ConnectionStrings[key].ConnectionString;
+        }
+
+        public void SaveConnectionString(string newConnection, string key)
+        {
+            config.ConnectionStrings.ConnectionStrings[key].ConnectionString = newConnection;
+            config.ConnectionStrings.ConnectionStrings[key].ProviderName = "System.Data.SQLite";
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
--- WindowsFormsApplication/Program.cs
+++ WindowsFormsApplication/Program.cs
@@ -11,10 +11,21 @@ namespace WindowsFormsApplication
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            string releaseConnectionString = @"Data Source=adanp.db";
+            string debugConnectionString = @"Data Source=..\..\..\DB\adanp.db";
+            ClassLibrary.AppSetting settings = new ClassLibrary.AppSetting();
+#if DEBUG
+            settings.SaveConnectionString(debugConnectionString, "AdanpConnectionString");
+#else
+            settings.SaveConnectionString(releaseConnectionString, "AdanpConnectionString");
+#endif
+
             Application.Run(new Form1());
         }
     }
